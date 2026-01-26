fd5d39670569a26b53215c650b3e3dd009d72359
Lucasbk38 <l.bk38@yahoo.fr>
2026-01-26 11:31:34 +0100

    better Traj3D yet again

--- gen/fitness.py
+++ gen/fitness.py
@@ -6,8 +6,8 @@ class Fitness():
     def __init__(self):
         pass
 
-    def evaluate(self, rot_table: Individu, traj: Traj3D, seq: str) -> float:
+    def evaluate(self, rot_table: Individu, traj: Traj3D, seq: str) -> np.floating:
         new_seq = seq + seq[0]
         assert(new_seq[0] == new_seq [-1])
 
-        return np.linalg.norm(traj.compute(new_seq,rot_table),2) #Norm of the last point of the DNA (we search to minimize it)
+        return np.linalg.norm(traj.compute(new_seq,rot_table), 2) #Norm of the last point of the DNA (we search to minimize it)
