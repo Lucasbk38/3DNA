fa232487f1d09685575626ad4eab1cfbc1c73158
Arthur Hakimi <arthur.hakimi@student-cs.fr>
2026-01-26 11:16:50 +0100

    Classe Fitness complétée

--- gen/fitness.py
+++ gen/fitness.py
@@ -1,5 +1,13 @@
 from individu import Individu
+from dna.Traj3D import Traj3D
+import numpy as np
 
 class Fitness():
-    def evaluate(self, rotTable: Individu) -> float:
+    def __init__(self):
         pass
+
+    def evaluate(self, rot_table: Individu, traj: Traj3D, seq: str) -> float:
+        new_seq = seq + seq[0]
+        assert(new_seq[0] == new_seq [-1])
+
+        return np.linalg.norm(traj.compute(new_seq,rot_table),2) #Norm of the last point of the DNA (we search to minimize it)
