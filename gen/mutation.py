64752918564952099ff2cd10d7afdb33fb3b46f6
Fypur <84095553+Fypur@users.noreply.github.com>
2026-01-27 10:15:53 +0100

    Added mutation probability

--- gen/mutation.py
+++ gen/mutation.py
@@ -2,10 +2,13 @@ from dna.RotTable import RotTable, rotTableConfig
 from typing import Callable
 from abc import ABC, abstractmethod
 import numpy as np
-
-
+from random import random
 
 class Mutation(ABC):
+    def __init__(self, mutation_probability: float) -> None:
+        super().__init__()
+        self.mutation_probability = mutation_probability
+
     @abstractmethod
     def mutateValue (self, e: float, delta: float) -> float:
         pass
@@ -15,10 +18,25 @@ class Mutation(ABC):
         pass
 
     def mutate(self, individu: RotTable) -> RotTable:
-        return RotTable({ k: [e if e >= 2 else float(np.clip( \
-            self.mutateValue(e, rotTableConfig[k][3 + i]), rotTableConfig[k][i] - rotTableConfig[k][3 + i], \
-            rotTableConfig[k][i] + rotTableConfig[k][3 + i])) for i, e in enumerate(dinuc)] \
-            for k, dinuc in individu.rot_table.items() })
+        rot_table_dict = {}
+
+        for k, dinuc in individu.rot_table.items():
+            rot_table_dict[k] = []
+            for i, e in enumerate(dinuc):
+                if e >= 2:
+                    rot_table_dict[k].append(e)
+                else:
+                    p = random()
+
+                    if p <= self.mutation_probability:
+                        center = rotTableConfig[k][i]
+                        delta = rotTableConfig[k][3 + i]
+                        mutated_value = self.mutateValue(e, delta)
+                        rot_table_dict[k].append(float(np.clip(mutated_value, center - delta, center + delta)))
+                    else:
+                        rot_table_dict[k].append(e)
+
+        return RotTable(rot_table_dict)
     
     def mutate_population(self, population: list[RotTable]):
         return [self.mutate(individu) for individu in population]
@@ -27,8 +45,8 @@ class Mutation(ABC):
 class GaussianMutation(Mutation):
     name = ""
 
-    def __init__(self, sigma=1.) -> None:
-        super().__init__()
+    def __init__(self, mutation_probability: float = 0.1, sigma=1.) -> None:
+        super().__init__(mutation_probability)
 
         self.sigma = sigma
 
@@ -57,13 +75,13 @@ class GaussianMultiplicativeMutation(GaussianMutation):
         return e * np.exp(self.gaussian())
     
 class SimulatedAnnealingMutation(Mutation):
-    def __init__(self, mutationClass) -> None:
-        super().__init__()
+    def __init__(self, mutationClass, mutation_probability= 0.1) -> None:
+        super().__init__(mutation_probability)
 
         self.mutationClass = mutationClass
 
     def __str__(self) -> str:
         return f"SA({ self.mutationClass.name })"
     
-    def mutateValue(self, *args) -> float:
-        return self.mutationClass().mutateValue(*args)
+    def mutateValue(self, e: float, delta: float) -> float:
+        return self.mutationClass(1).mutateValue(e, delta)
