e6fa4ff21756a0ac165449446082714614923d1e
Lucasbk38 <l.bk38@yahoo.fr>
2026-01-28 09:01:04 +0100

    pas grand chose

--- gen/__main__.py
+++ gen/__main__.py
@@ -5,7 +5,7 @@ from gen.genetic import *
 
 
 benchmark(
-    2048, 64, 1/4, 1/8,
+    2048, 64, 0, 1/4,
     "data/plasmid_8k.fasta",
     [ TournamentWithHopeSelection(hopeProbability=.01) ],
     [ ThresholdMutation(SimulatedAnnealingMutation(GaussianAdditiveDeltaMutation(sigma=1), key="sigma", alpha=.995), mutation_probability=.2) for _ in range(1) ],
