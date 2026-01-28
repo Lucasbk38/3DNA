6d29e27a1e5dc09e632b2f2f278912781dad1650
Arthur Hakimi <arthur.hakimi@student-cs.fr>
2026-01-28 10:44:20 +0100

    Correction

--- gen/genetic.py
+++ gen/genetic.py
@@ -126,3 +126,4 @@ def benchmark(
     fichier = os.path.join(dir, f"{nb_nucleotide}nucleotide_{best_fitness}.json")
     with open(fichier, 'w') as file:
                 best_rottable.rot_table["score"] = best_fitness
+                json_dump(best_rottable.rot_table,file,indent = 4)
