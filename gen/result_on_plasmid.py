b48404775ba3d5c49346f41f29841a8452922d41
Arthur Hakimi <arthur.hakimi@student-cs.fr>
2026-01-28 09:00:27 +0100

    print du résultat

--- gen/result_on_plasmid.py
+++ gen/result_on_plasmid.py
@@ -8,7 +8,7 @@ def any_rottable_result(rottable_filename : str,seq_filename: str):
     traj = Traj3D()
     lineList = [line.rstrip('\n') for line in open(seq_filename)]
     seq = ''.join(lineList[1:])
-    return fit.evaluate(RotTable(rottable),traj,seq)
+    print(fit.evaluate(RotTable(rottable),traj,seq))
 
 def best_rottable_8k():
     any_rottable_result("gen/best_rottable8k.json","data/plasmid_8k.fasta")
